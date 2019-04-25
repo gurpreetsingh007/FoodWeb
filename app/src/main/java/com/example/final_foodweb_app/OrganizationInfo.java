6cea33ff013c8f771efbae20e9ab8e6544cb042d
Gurpreet <gskohaar@gmail.com>
2019-04-25 16:29:02 -0700

    forgot fixed again

--- app/src/main/java/com/example/final_foodweb_app/OrganizationInfo.java
+++ app/src/main/java/com/example/final_foodweb_app/OrganizationInfo.java
@@ -3,8 +3,11 @@ package com.example.final_foodweb_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class OrganizationInfo extends AppCompatActivity {
+    private Button available_items, view_pickups;
 
 
     @Override
@@ -13,6 +16,16 @@ public class OrganizationInfo extends AppCompatActivity {
         setContentView(R.layout.organizationinfo);
         Intent intent = getIntent();
         String passing = intent.getStringExtra("login");
+        available_items = findViewById(R.id.available_button);
+        view_pickups = findViewById(R.id.view_pickups);
+        available_items.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrganizationInfo.this, Organizationitems.class);
+                intent.putExtra("items","passing");
+                startActivity(intent);
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/final_foodweb_app/Organizationitems.java
@@ -0,0 +1,17 @@
+package com.example.final_foodweb_app;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Organizationitems extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
