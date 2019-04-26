b8ecfadd2c011fcaa067ada66b191f8563d5e42c
Gurpreet <gskohaar@gmail.com>
2019-04-25 22:32:15 -0700

    recycler for view items done

---
+++ app/src/main/java/com/example/final_foodweb_app/MyAdapter.java
@@ -0,0 +1,87 @@
+package com.example.final_foodweb_app;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.text.BreakIterator;
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+
+    private ArrayList<String> names;
+    private ArrayList<String> images;
+    private ArrayList<Double> distances;
+    private Context mContext;
+
+
+    public MyAdapter(Context mContext,ArrayList<String> names, ArrayList<String> images,
+                     ArrayList<Double> distances) {
+        this.names = names;
+        this.images = images;
+        this.mContext = mContext;
+        this.distances = distances;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclerview,viewGroup,false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(images.get(position))
+                .into(holder.image);
+        holder.name.setText(names.get(position));
+        holder.distance.setText((distances.get(position).toString())+" miles away");
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext,names.get(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return names.size();
+    }
+
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView name,distance;
+        public CircleImageView image;
+        RelativeLayout parentLayout;
+
+        public MyViewHolder(View v) {
+            super(v);
+            name = v.findViewById(R.id.name);
+            distance = v.findViewById(R.id.distance);
+            image = v.findViewById(R.id.image);
+            parentLayout = v.findViewById(R.id.parent_layout);
+        }
+    }
+
+}
--- app/src/main/java/com/example/final_foodweb_app/Organizationitems.java
+++ app/src/main/java/com/example/final_foodweb_app/Organizationitems.java
@@ -1,17 +1,85 @@
 package com.example.final_foodweb_app;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Organizationitems extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private String[] myDataset;
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<Double> mdistance = new ArrayList<>();
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.organizationitems);
+        Intent intent = getIntent();
+        String passing = intent.getStringExtra("items");
+        initImageBitmaps();
+
+//        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+//        recyclerView.setHasFixedSize(true);
+//
+//        layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+//
+//        myDataset = new String[]{"hello"};
+//
+//        mAdapter = new MyAdapter(names,images,distance);
+//        recyclerView.setAdapter(mAdapter);
 
 
 
     }
+
+    private void initImageBitmaps(){
+        mImageUrls.add("https://i.redd.it/mgbymkbaleu21.jpg");
+        mNames.add("Tandori night");
+        mdistance.add(2.3);
+
+        mImageUrls.add("https://i.redd.it/4qy2aygceau21.jpg");
+        mNames.add("Taco bell");
+        mdistance.add(3.1);
+
+        mImageUrls.add("https://i.redd.it/nu6sdx4ydiu21.jpg");
+        mNames.add("Chaat cafe");
+        mdistance.add(1.5);
+
+        mImageUrls.add("https://i.redd.it/zmio0th7eeu21.jpg");
+        mNames.add("Thai Noodles");
+        mdistance.add(2.1);
+
+        mImageUrls.add("https://i.redd.it/c0xkavo6egu21.jpg");
+        mNames.add("Viks");
+        mdistance.add(4.1);
+
+        mImageUrls.add("https://i.redd.it/6g0wx2otugu21.jpg");
+        mNames.add("Mehak chaat");
+        mdistance.add(0.5);
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.my_recycler_view);
+        MyAdapter adapter = new MyAdapter(this,mNames,mImageUrls,mdistance);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
 }
